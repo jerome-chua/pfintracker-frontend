c320ff88aa35c935066eed0ea646026247482592
Jchu4 <jerome.chuame@gmail.com>
2021-06-13 10:54:02 +0800

    Update main app to include router paths

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,32 +1,39 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 import Login from "./Login.jsx";
 
-import { GoogleSpreadsheet } from "google-spreadsheet";
-import creds from "../client_secret.js";
-
-const SHEET_ID = "15CWgsB3aTdEcY_dR7pnCOR70SxQP7LljEwGTxfaQOZo";
-const doc = new GoogleSpreadsheet(SHEET_ID);
-
-async function accessSheets() {
-  await doc.useServiceAccountAuth({
-    client_email: creds.client_email,
-    private_key: creds.private_key,
-  });
-
-  await doc.loadInfo(); // loads document properties and worksheets
-  console.log("Doc", doc.title);
-
-  const sheet = doc.sheetsByIndex[0]; // or use doc.sheetsById[id]
-  console.log(`Title: ${sheet.title}, Rows: ${sheet.rowCount}`);
-}
-
 export default function App() {
-  console.log("test");
-  accessSheets();
+  console.log("Main App Renders");
 
   return (
-    <div>
-      <Login />
-    </div>
+    <Router>
+      <div>
+        {/*TODO: Cater for 2 different navbars */}
+        {/*TODO: Update to Material UI AppBar */}
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/transactions">Transactions</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Dashboard</Link>
+            </li>
+            <li>
+              <Link to="/dashboard">Budget</Link>
+            </li>
+          </ul>
+        </nav>
+
+        <Switch>
+          <Route path="/transactions">Transactions Component</Route>
+          <Route path="/dashboard">Dashboard Component</Route>
+          <Route path="/budget">Budget Component</Route>
+          <Route path="/">Home Component</Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
--- src/components/__tests__/App.test.js
+++ src/components/__tests__/App.test.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import App from '../App.jsx';
 
 it('shows a Login form', () => {
-
   // Create div ele in JSDom
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
