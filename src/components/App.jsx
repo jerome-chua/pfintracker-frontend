d6d30f0a88cc0b5ed7709e34b754b5f1fc995823
Jchu4 <jerome.chuame@gmail.com>
2021-06-13 11:34:17 +0800

    Remove window prop from UserHeader. Remove lorem ipsum text from App. Install material-ui-styles

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,14 +9,6 @@ export default function App() {
   return (
     <Router>
       <UserHeader />
-      {[...new Array(120)]
-        .map(
-          () => `Cras mattis consectetur purus sit amet fermentum.
-Cras justo odio, dapibus ac facilisis in, egestas eget quam.
-Morbi leo risus, porta ac consectetur ac, vestibulum at eros.
-Praesent commodo cursus magna, vel scelerisque nisl consectetur et.`
-        )
-        .join("\n")}
       <div>
         {/*TODO: Cater for 2 different navbars */}
         {/*TODO: Update to Material UI AppBar */}
