a1eee4b0dc252ba4d7bb82d2b2214417b99022e9
Jchu4 <jerome.chuame@gmail.com>
2021-06-14 22:57:27 +0800

    Add material-ui date picker

--- src/components/Datepicker.jsx
+++ src/components/Datepicker.jsx
@@ -1,13 +1,41 @@
 import React, { useState } from "react";
+import "date-fns";
+import { Grid } from "@material-ui/core";
+import DateFnsUtils from "@date-io/date-fns";
+import {
+  MuiPickersUtilsProvider,
+  KeyboardTimePicker,
+  KeyboardDatePicker,
+} from "@material-ui/pickers";
 
 export default function Datepicker() {
   const [selectedDate, setSelectedDate] = useState(
-    new Date("2014-08-18T21:11:54")
+    new Date("2021-06-18T21:11:54")
   );
 
   const handleDateChange = (date) => {
     setSelectedDate(date);
   };
 
-  return <></>;
+  return (
+    <>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        <Grid container justify="space-around">
+          <KeyboardDatePicker
+            disableToolbar
+            variant="inline"
+            format="MM/dd/yyy"
+            margin="normal"
+            id="date-picker"
+            label="Date Picker"
+            value={selectedDate}
+            onChange={handleDateChange}
+            KeyboardButtonProps={{
+              "aria-label": "change date",
+            }}
+          ></KeyboardDatePicker>
+        </Grid>
+      </MuiPickersUtilsProvider>
+    </>
+  );
 }
