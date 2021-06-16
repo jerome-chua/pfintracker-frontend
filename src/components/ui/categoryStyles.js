19aee6c8322f0bf7488c625359efa119e3dec92d
Jchu4 <jerome.chuame@gmail.com>
2021-06-16 11:55:54 +0800

    Update categorystyles naming convention. Create a custom hook

--- src/components/ui/categoryStyles.js
+++ src/components/ui/categoryStyles.js
@@ -1,32 +1,32 @@
 const expenseColors = ['#b50d12', '#bf2f1f', '#c9452c', '#d3583a', '#dc6a48', '#e57c58', '#ee8d68', '#f79d79', '#ffae8a', '#cc474b', '#f55b5f', '#A0937D', '#5F939A'];
 
 export const expenseCategories = [
-  { type: 'Bills', amount: 0, color: expenseColors[0] },
-  { type: 'Transportation', amount: 0, color: expenseColors[1] },
-  { type: 'Shopping', amount: 0, color: expenseColors[2] },
-  { type: 'Sports', amount: 0, color: expenseColors[3] },
-  { type: 'Food & Drinks', amount: 0, color: expenseColors[4] },
-  { type: 'Books', amount: 0, color: expenseColors[5] },
-  { type: 'House', amount: 0, color: expenseColors[6] },
-  { type: 'Entertainment', amount: 0, color: expenseColors[7] },
-  { type: 'Investment', amount: 0, color: expenseColors[8] },
-  { type: 'Church', amount: 0, color: expenseColors[9] },
-  { type: 'Stationery', amount: 0, color: expenseColors[10] },
-  { type: 'Haircuts', amount: 0, color: expenseColors[11] },
-  { type: 'Church', amount: 0, color: expenseColors[12] },
+  { category: 'Bills', amount: 0, color: expenseColors[0] },
+  { category: 'Transportation', amount: 0, color: expenseColors[1] },
+  { category: 'Shopping', amount: 0, color: expenseColors[2] },
+  { category: 'Sports', amount: 0, color: expenseColors[3] },
+  { category: 'Food & Drinks', amount: 0, color: expenseColors[4] },
+  { category: 'Books', amount: 0, color: expenseColors[5] },
+  { category: 'House', amount: 0, color: expenseColors[6] },
+  { category: 'Entertainment', amount: 0, color: expenseColors[7] },
+  { category: 'Investment', amount: 0, color: expenseColors[8] },
+  { category: 'Church', amount: 0, color: expenseColors[9] },
+  { category: 'Stationery', amount: 0, color: expenseColors[10] },
+  { category: 'Haircuts', amount: 0, color: expenseColors[11] },
+  { category: 'Church', amount: 0, color: expenseColors[12] },
 ];
 
 const incomeColors = ['#123123', '#154731', '#165f40', '#16784f', '#14915f', '#10ac6e', '#0bc77e', '#04e38d'];
 
 export const incomeCategories = [
-  { type: 'Business', amount: 0, color: incomeColors[0] },
-  { type: 'Investments', amount: 0, color: incomeColors[1] },
-  { type: 'Extra Income', amount: 0, color: incomeColors[2] },
-  { type: 'Deposits', amount: 0, color: incomeColors[3] },
-  { type: 'Rental Income', amount: 0, color: incomeColors[4] },
-  { type: 'Gifts', amount: 0, color: incomeColors[5] },
-  { type: 'Salary', amount: 0, color: incomeColors[6] },
-  { type: 'Savings', amount: 0, color: incomeColors[7] },
+  { category: 'Business', amount: 0, color: incomeColors[0] },
+  { category: 'Investments', amount: 0, color: incomeColors[1] },
+  { category: 'Extra Income', amount: 0, color: incomeColors[2] },
+  { category: 'Deposits', amount: 0, color: incomeColors[3] },
+  { category: 'Rental Income', amount: 0, color: incomeColors[4] },
+  { category: 'Gifts', amount: 0, color: incomeColors[5] },
+  { category: 'Salary', amount: 0, color: incomeColors[6] },
+  { category: 'Savings', amount: 0, color: incomeColors[7] },
 ];
 
 export const resetCategories = () => {
--- src/useCategories.js
+++ src/useCategories.js
@@ -1,17 +1,38 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import { savifyContext } from "./store.js";
+import { incomeCategories, expenseCategories, resetCategories } from './ui/categoryStyles.js';
 
 
 export default function useCategories(type) {
+  resetCategories();
+
   const {store, } = useContext(savifyContext);
   const { transactions } = store;
 
+  // Filter for income | expense
   const chosenTransacts = transactions.filter((row) => row.transactionType === type)
 
+  // Calculate total amount for income | expense
   const total = chosenTransacts.reduce((acc, currVal) => 
     acc += currVal.amount, 0
   );
 
+  // Filter for income | expense typed categories
   const categories = type === 'Income' ? incomeCategories : expenseCategories;
+  chosenTransacts.forEach((transaction) => {
+    // Return first category found
+    const category = categories.find((cat) => cat.category === transaction.category);
+
+    if (category) {
+      category.amount += transaction.amount;
+    }
+  });
+
+  const filteredCategories = categories.filter((cat) => cat.amount > 0);
+
+
+
+  return { filteredCategories, total };
+};
 
 }
