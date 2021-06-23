78ba35e9165d12d300d25b3203de53582d9a744b
Jchu4 <jerome.chuame@gmail.com>
2021-06-23 17:06:10 +0800

    Create a caclulation specific to date range

--- src/components/charts/TimeChart.jsx
+++ src/components/charts/TimeChart.jsx
@@ -6,16 +6,20 @@ import { ToggleButtonGroup, ToggleButton } from "@material-ui/lab";
 import { Line } from "react-chartjs-2";
 import useStyles from "./styles";
 import useCategories from "../../useCategories.js";
+import moment from "moment";
 
 const fixedDecimal = (x) => {
   return Number(Number.parseFloat(x).toFixed(2));
 };
 
-export default function TimeChart({ type }) {
-  console.log("Time Chart renders");
+const strFormat = (date) => {
+  return moment(date).format("YYYY-MM-DD");
+};
 
+export default function TimeChart({ type }) {
   const { store, dispatch } = useContext(SavifyContext);
   const { transactions, dateRange } = store;
+  const { startDate, endDate } = dateRange;
   const classes = useStyles();
   const { timeData } = useCategories(type);
   const [period, setPeriod] = useState("day"); // Sets "day", week", "month"
@@ -32,6 +36,9 @@ export default function TimeChart({ type }) {
     },
   };
 
+  const momentStart = strFormat(startDate);
+  const momentEnd = strFormat(endDate);
+
   // Calculate total amount for income | expense
   const total = transactions.reduce(
     (acc, currVal) =>
@@ -41,8 +48,20 @@ export default function TimeChart({ type }) {
     0
   );
 
-  console.log(typeof fixedDecimal(total));
-  console.log(typeof total);
+  const filteredTransactions = transactions.filter((currVal) => {
+    return moment(strFormat(currVal.createdAt)).isBetween(
+      momentStart,
+      momentEnd
+    );
+  });
+
+  const filteredTotal = filteredTransactions.reduce(
+    (acc, currVal) =>
+      currVal.transactionType === "Income"
+        ? acc + currVal.amount
+        : acc - currVal.amount,
+    0
+  );
 
   return (
     <Card className={type === "Income" ? classes.income : classes.expense}>
@@ -78,7 +97,11 @@ export default function TimeChart({ type }) {
         <Grid item xs={12}>
           <CardContent>
             <Typography variant="h5">
-              ${fixedDecimal(total).toLocaleString()}
+              Total Savings: ${fixedDecimal(total).toLocaleString()}
+            </Typography>
+            <Typography variant="h5">
+              Date Range Specific: $
+              {fixedDecimal(filteredTotal).toLocaleString()}
             </Typography>
             <Line data={timeData} options={options} />
           </CardContent>
